74c45f9739168355c5ea311ff663196c5e2b8b5a
maggioniduffy <fausmaggioni5@gmail.com>
2025-02-20 16:29:02 -0300

    some more details on start up page

---
+++ components/Ping.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ping = () => {
+  return <div>Ping</div>;
+};
+
+export default Ping;
---
+++ components/View.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  id: string;
+}
+const View = ({ id }: Props) => {
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2"></div>
+    </div>
+  );
+};
+
+export default View;
